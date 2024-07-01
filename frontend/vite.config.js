bc0bf30d064b477dc8a103d28d24d6bed86b539e
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-07-01 16:36:58 +0200

    Testing .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   base: '/virtual-dojo/frontend/',
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
