1a067baabf94236babf8ecd40e85226b89c53da9
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-07-02 15:18:46 +0200

    Adding messages in email validation wit redirect to login

--- frontend/src/pages/validation/Validation.jsx
+++ frontend/src/pages/validation/Validation.jsx
@@ -1,20 +1,16 @@
-import { useLocation } from 'react-router-dom'
-import { useState } from "react"
-import { Dialog } from 'primereact/dialog'
-import { Button } from 'primereact/button'
-import { useValidation } from '../../hooks/useValidation'
-import { useCountdown } from '../../hooks/useCountdown'
+import {useLocation} from 'react-router-dom'
+import {useState} from "react"
+import {Dialog} from 'primereact/dialog'
+import {Button} from 'primereact/button'
+import {useValidation} from '../../hooks/useValidation'
+import {useCountdown} from '../../hooks/useCountdown'
 
 export default function Validation() {
     const location = useLocation()
     const params = new URLSearchParams(location.search)
     const mail = params.get('mail')
-    const { state, errorMessage } = useValidation(mail)
-    const countdown = useCountdown(30, state === 'succeeded')
-
-    function closeDialog() {
-        setState('')
-    }
+    const {state, errorMessage} = useValidation(mail)
+    const countdown = useCountdown(15, state === 'succeeded')
 
     return (
         <div>
@@ -22,13 +18,15 @@ export default function Validation() {
                 header="Estado de Validación"
                 visible={state !== ''}
                 modal
-                onHide={() => closeDialog()}
+                onHide={() => {
+                }}
                 footer={
                     <div>
-                        <Button label="Aceptar" onClick={() => closeDialog()} autoFocus />
+                        <Button label="Aceptar" icon="pi pi-check" onClick={() => {
+                            window.location.href = '/virtual-dojo/frontend/login'
+                        }}/>
                     </div>
-                }
-            >
+                }>
                 {state === 'succeeded' && <p>Validación exitosa. Redirigiendo al login en {countdown} segundos...</p>}
                 {state === 'canceled' && <p>{errorMessage}</p>}
                 {!state && <p>Estado desconocido</p>}
